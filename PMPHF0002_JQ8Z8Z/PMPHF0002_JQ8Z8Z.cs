a7ea554bcca55a7e3f47ef36d86adec72fa79be8
Rille Soma <rillesoma75@gmail.com>
2023-10-13 18:40:40 +0200

    upload

---
+++ PMPHF0002_JQ8Z8Z/PMPHF0002_JQ8Z8Z.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMPHF0002_JQ8Z8Z
+{
+    internal class PMPHF0002_JQ8Z8Z
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string C = Console.ReadLine();
+            Console.Clear();
+            for(int i = 0; i < N; i++)
+            {
+                Console.Write(C);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < N - 2; i++)
+            {
+                Console.Write(C);
+                for(int j = 0; j < N - 2;j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(C);
+                Console.WriteLine();
+            }
+            if ( N > 1)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Console.Write(C);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
